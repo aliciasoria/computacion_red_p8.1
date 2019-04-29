c177b9098bb9c0b837511f763d0700523db8ca66
aliciasoria <alicia.soria.gomez@alumnos.upm.es>
2019-04-29 22:00:06 +0200

    oke

--- quiz_express/models/index.js
+++ quiz_express/models/index.js
@@ -1,19 +1,41 @@
-const Sequelize = require('sequelize');
-const Op = Sequelize.Op;
 const url = process.env.DATABASE_URL || "sqlite:quiz.sqlite";
 
+
+
+const Sequelize = require('sequelize');
+const Op = Sequelize.Op;
+
 const options = {logging: false};
-const sequelize = new Sequelize(url);//options
+const sequelize = new Sequelize("sqlite:quizzes.sqlite", options);
 
 const Quiz = sequelize.define(  // define Quiz model (table quizzes)
     'quiz',
     {
         question: Sequelize.STRING,
-        answer:Sequelize.STRING
+        answer: Sequelize.STRING
     }
 );
 
-  const Players = sequelize.define(  // define Play model (table players)
+sequelize.sync() // Syncronize DB and seed if needed
+    .then(() => Quiz.count())
+    .then(count => {
+        if (count === 0) {
+            return Quiz.bulkCreate([
+                {question: "Capital of Italy", answer: "Rome"},
+                {question: "Capital of France", answer: "Paris"},
+                {question: "Capital of Spain", answer: "Madrid"},
+                {question: "Capital of Portugal", answer: "Lisbon"}
+            ])
+                .then(c => console.log(`DB filled with ${c.length} quizzes.`));
+        } else {
+            console.log(`DB exists & has ${count} quizzes.`);
+        }
+    })
+    .catch(console.log);
+
+
+
+    const Players = sequelize.define(  // define Play model (table players)
         'player',
         {
             name: Sequelize.STRING,
@@ -21,11 +43,20 @@ const Quiz = sequelize.define(  // define Quiz model (table quizzes)
             score:Sequelize.INTEGER
         }
     );
-    sequelize.sync()
-    .then(() => console.log('Data Bases created successfully'))
-    .catch(error => {
-        console.log("Error creating the data base tables:", error);
-        process.exit(1);
-    });
+
+    sequelize.sync() // Syncronize DB and seed if needed
+        .then(() => Players.count())
+        .then(count => {
+            if (count === 0) {
+                return Players.bulkCreate([
+                    {name: "Alicia", password: "123",score:0}
+
+                ])
+                    .then(c => console.log(`Players filled with ${c.length} player.`));
+            } else {
+                console.log(`Player exists & has ${count} players.`);
+            }
+        })
+        .catch(console.log);
 
     module.exports=sequelize;
